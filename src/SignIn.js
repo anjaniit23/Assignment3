f46b24ec3c8b10a469d56f1ecd56759668454c69
anjaniit23 <84245319+anjaniit23@users.noreply.github.com>
2021-07-26 11:42:53 +0530

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,34 +1,19 @@
   
 import React from 'react';
+import { findAllInRenderedTree } from 'react-dom/test-utils';
 import './Signup.css';
 
 
   
 const Signin = () => {
-      let inputsigninemail=null;
-      let inputsigninpassword=null;
-    const verificationSignIn = () =>{
-        if((inputsigninemail===null)||(inputsigninpassword===null)){
-          alert("Do not leave any field Empty");
-        }
-        else{
-           alert("signed In successfully");
-        }
-       
-      } 
-      const  Inputsigninemail = (e) =>{
-        inputsigninemail=e.target.value;
-      }
-      const  Inputsigninpassword = (e) =>{
-        inputsigninpassword=e.target.value;
-      }
+
         return (
                 <div> 
                   <div className="message"></div>
-                  <form onSubmit={verificationSignIn}>
+                  <form action="alert('hey')">
                    <div className="signin">
-                       <div className="textsignin"><span >Enter Email Add.</span><input className="inputboxsignin" type="email" onChange={Inputsigninemail} required /></div>
-                       <div className="textsignin"><span >Enter Password</span><input className="inputboxsignin" type="password" pattern=".{8,}" title="Should contain 8 or more chars" onChange={Inputsigninpassword} required /> </div> 
+                       <div className="textsignin"><span >Enter Email Add.</span><input className="inputboxsignin" type="email"  required /></div>
+                       <div className="textsignin"><span >Enter Password</span><input className="inputboxsignin" type="password" pattern=".{8,}" title="Should contain 8 or more chars"  required /> </div> 
                        <div className="btnsignin"><input type="submit" /></div>
                   </div> 
                   </form>
