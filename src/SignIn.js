411ac976d7b11729bb98e176adc9e35e8a78da64
anjaniit23 <84245319+anjaniit23@users.noreply.github.com>
2021-07-26 12:37:46 +0530

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -6,13 +6,11 @@ import './Signup.css';
 
   
 const Signin = () => {
-        const messagesignin=()=>{
-          alert("Signed In successfully");
-        }
+  
         return (
                 <div> 
                   <div className="message"></div>
-                  <form onSubmit={ messagesignin}>
+                  <form action="https://www.betterteam.com/i/thank-you-letter-to-employees-1024x512-20190212-1-1.jpg">
                    <div className="signin">
                        <div className="textsignin"><span >Enter Email Add.</span><input className="inputboxsignin" type="email"  required /></div>
                        <div className="textsignin"><span >Enter Password</span><input className="inputboxsignin" type="password" pattern=".{8,}" title="Should contain 8 or more chars"  required /> </div> 
