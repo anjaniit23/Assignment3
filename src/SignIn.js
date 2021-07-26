c219567996f1c413d682da97daf6791942c5f04d
anjaniit23 <84245319+anjaniit23@users.noreply.github.com>
2021-07-26 11:48:30 +0530

    Update SignIn.js

--- src/SignIn.js
+++ src/SignIn.js
@@ -1,6 +1,6 @@
   
 import React from 'react';
-import { findAllInRenderedTree } from 'react-dom/test-utils';
+// import { findAllInRenderedTree } from 'react-dom/test-utils';
 import './Signup.css';
 
 
@@ -10,7 +10,7 @@ const Signin = () => {
         return (
                 <div> 
                   <div className="message"></div>
-                  <form action="alert('hey')">
+                  <form onSubmit={alert("Signed In successfully!")}>
                    <div className="signin">
                        <div className="textsignin"><span >Enter Email Add.</span><input className="inputboxsignin" type="email"  required /></div>
                        <div className="textsignin"><span >Enter Password</span><input className="inputboxsignin" type="password" pattern=".{8,}" title="Should contain 8 or more chars"  required /> </div> 
